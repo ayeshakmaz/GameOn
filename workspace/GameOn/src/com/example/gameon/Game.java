2ebc00efecefa935b417542f3e8a530ed65b1b97
aptran <tran.anh.p@gmail.com>
2013-11-01 23:04:01 -0700

    added method to save Game object to database

--- workspace/GameOn/src/com/example/gameon/Game.java
+++ workspace/GameOn/src/com/example/gameon/Game.java
@@ -1,48 +1,78 @@
 package com.example.gameon;
 
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 public class Game {
 
+	private ParseUser user;
 	private Sport sport;
-	private String place;
+	private String location;
 	private DayAndTime dat;
 	private String gender;
 	private String size;
 	
 
-	public Game(Sport sport, String place, DayAndTime dat, String gender, String size) {
+	public Game(ParseUser user, Sport sport, String location, DayAndTime dat, String gender, String size) {
+		this.user = user;
 		this.sport = sport;
-		this.place = place;
+		this.location = location;
 		this.dat = dat;
 		this.gender = gender;
 		this.size = size;
 	}
 	
+	public boolean addGame(Game game) {
+		ParseObject gameobj = new ParseObject("game");
+		gameobj.put("user", game.getUser().getUsername());
+		gameobj.put("sport_type", game.getSport().getName());
+		gameobj.put("location", game.getLocation());
+		gameobj.put("date", game.getDAT().getDate()+" "+game.getDAT().getTime());
+		gameobj.put("gender", game.getGender());
+		gameobj.put("size", game.getSize());
+		
+		ParseObject sportobj = new ParseObject("sport");
+		sportobj.put("name", game.getSport().getName());
+		sportobj.put("level", game.getSport().getLevel());
+		
+		gameobj.saveInBackground();
+		sportobj.saveInBackground();
+		return true;		
+	}
+	
+	
+	
+	
+	public ParseUser getUser(){
+		return this.user;
+	}
+	
 	public Sport getSport() {
-		return sport;
+		return this.sport;
 	}
 
 	public void setSport(Sport sport) {
 		this.sport = sport;
 	}
 
-	public String getPlace() {
-		return place;
+	public String getLocation() {
+		return this.location;
 	}
 
 	public void setPlace(String place) {
-		this.place = place;
+		this.location = place;
 	}
 
-	public DayAndTime getDat() {
-		return dat;
+	public DayAndTime getDAT() {
+		return this.dat;
 	}
 
-	public void setDat(DayAndTime dat) {
+	public void setDAT(DayAndTime dat) {
 		this.dat = dat;
 	}
 
 	public String getGender() {
-		return gender;
+		return this.gender;
 	}
 
 	public void setGender(String gender) {
