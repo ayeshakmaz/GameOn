6c9580e4827ed0317623567739d9eb2d729a3511
rasad88 <rasad88@gmail.com>
2013-11-01 19:50:06 -0700

    New workspace API 19

---
+++ workspace/GameOn/src/com/example/gameon/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gameon;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Parse.initialize(this, "TpTvZ7H4ABQPG9ig5Io3lko0VcUVlOZpDiQXHZuj", "1p4926LkDGfpKY5PEgiJwGejMgYMUSWPtaBaDPPX");
+        ParseAnalytics.trackAppOpened(getIntent());
+        
+        ParseObject testObject = new ParseObject("game");
+        testObject.put("name", "anh");
+        testObject.saveInBackground();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
