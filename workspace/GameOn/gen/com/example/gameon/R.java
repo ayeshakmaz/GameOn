b79e21cdd79d61eae3167776a424476c78cd27f8
ayeshakmaz <ayeshamazumdar@128.54.119.77>
2013-11-09 01:28:15 -0800

    Slide menu

--- workspace/GameOn/gen/com/example/gameon/R.java
+++ workspace/GameOn/gen/com/example/gameon/R.java
@@ -27,32 +27,40 @@ public final class R {
     public static final class drawable {
         public static final int home_screen=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int main_bg=0x7f020002;
+        public static final int menu=0x7f020003;
+        public static final int tile_bg_avg=0x7f020004;
+        public static final int tile_bg_nub=0x7f020005;
+        public static final int tile_bg_pro=0x7f020006;
     }
     public static final class id {
-        public static final int DAT_textView_gridView=0x7f09000d;
-        public static final int RelativeLayout=0x7f09000c;
-        public static final int action_settings=0x7f090011;
-        public static final int button1=0x7f09000b;
-        public static final int email_editText_signup=0x7f09000a;
-        public static final int gender_textView_gridView=0x7f09000f;
-        public static final int gridview=0x7f090005;
-        public static final int location_textView_gridView=0x7f09000e;
+        public static final int DAT_textView_gridView=0x7f09000e;
+        public static final int RelativeLayout=0x7f09000d;
+        public static final int action_settings=0x7f090012;
+        public static final int button1=0x7f090009;
+        public static final int email_editText_signup=0x7f09000c;
+        public static final int gender_textView_gridView=0x7f090010;
+        public static final int gridview=0x7f090006;
+        public static final int location_textView_gridView=0x7f09000f;
         public static final int login_button=0x7f090002;
-        public static final int need_textView_gridView=0x7f090010;
+        public static final int menu_button=0x7f090004;
+        public static final int need_textView_gridView=0x7f090011;
         public static final int password_editText=0x7f090000;
-        public static final int password_editText_signup=0x7f090009;
+        public static final int password_editText_signup=0x7f09000b;
         public static final int signup_button=0x7f090003;
-        public static final int spinner1=0x7f090007;
-        public static final int spinner2=0x7f090006;
-        public static final int sport_textView_gridView=0x7f090004;
+        public static final int spinner1=0x7f090008;
+        public static final int spinner2=0x7f090007;
+        public static final int sport_textView_gridView=0x7f090005;
         public static final int username_editText=0x7f090001;
-        public static final int username_editText_signup=0x7f090008;
+        public static final int username_editText_signup=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_signup=0x7f030002;
-        public static final int grid_tile=0x7f030003;
+        public static final int grid_tile_avg=0x7f030003;
+        public static final int grid_tile_nub=0x7f030004;
+        public static final int grid_tile_pro=0x7f030005;
     }
     public static final class menu {
         public static final int login=0x7f080000;
--- workspace/GameOn/src/com/example/gameon/MainActivity.java
+++ workspace/GameOn/src/com/example/gameon/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 
 import com.example.gameon.sports.Basketball;
 import com.example.gameon.sports.Football;
+import com.example.gameon.view.viewgroup.FlyOutContainer;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
@@ -24,59 +25,77 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ArrayList<Game> gArr = new ArrayList<Game>();
-        CustomGridViewAdapter customGridAdapter;
-        Parse.initialize(this, "TpTvZ7H4ABQPG9ig5Io3lko0VcUVlOZpDiQXHZuj", "1p4926LkDGfpKY5PEgiJwGejMgYMUSWPtaBaDPPX"); 
-        ParseAnalytics.trackAppOpened(getIntent());
-        
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        if (currentUser != null) {
-          // do stuff with the user
-        } else {
-          // show the signup or login screen
-        	Intent intent = new Intent(this, LoginActivity.class);
-    		startActivity(intent);
-        }
-        
-        addListenerOnSpinnerItemSelection();
-    	
-    	GridView gridview = (GridView) findViewById(R.id.gridview);
-    	
-        gArr.add(new Game(currentUser, new Football("nub"),"here", new DayAndTime(new Date(), new Time(3, 0, 0)),"Male", "5"));
-        gArr.add(new Game(currentUser, new Basketball("avg"),"here", new DayAndTime(new Date(), new Time(12, 0, 0)),"Female", "5"));
-        gArr.add(new Game(currentUser, new Football("pro"),"here", new DayAndTime(new Date(), new Time(3, 0, 0)),"Male", "3"));
-        gArr.add(new Game(currentUser, new Basketball("pro"),"here", new DayAndTime(new Date(), new Time(12, 0, 0)),"Male", "5"));
-        gArr.add(new Game(currentUser, new Basketball("avg"),"here", new DayAndTime(new Date(), new Time(12, 0, 0)),"Male", "5"));
-        gArr.add(new Game(currentUser, new Basketball("nub"),"here", new DayAndTime(new Date(), new Time(12, 0, 0)),"Male", "5"));
-    	
-    	customGridAdapter = new CustomGridViewAdapter(this, R.layout.grid_tile_nub, gArr);
-        gridview.setAdapter(customGridAdapter);
-        
-        gridview.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
-        
-    }
+	FlyOutContainer root;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.root = (FlyOutContainer) this.getLayoutInflater().inflate(
+				R.layout.activity_main, null);
+		this.setContentView(root);
+
+		ArrayList<Game> gArr = new ArrayList<Game>();
+		CustomGridViewAdapter customGridAdapter;
+		Parse.initialize(this, "TpTvZ7H4ABQPG9ig5Io3lko0VcUVlOZpDiQXHZuj",
+				"1p4926LkDGfpKY5PEgiJwGejMgYMUSWPtaBaDPPX");
+		ParseAnalytics.trackAppOpened(getIntent());
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// do stuff with the user
+		} else {
+			// show the signup or login screen
+			Intent intent = new Intent(this, LoginActivity.class);
+			startActivity(intent);
+		}
+
+		addListenerOnSpinnerItemSelection();
+
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+
+		gArr.add(new Game(currentUser, new Football("nub"), "here",
+				new DayAndTime(new Date(), new Time(3, 0, 0)), "Male", "5"));
+		gArr.add(new Game(currentUser, new Basketball("avg"), "here",
+				new DayAndTime(new Date(), new Time(12, 0, 0)), "Female", "5"));
+		gArr.add(new Game(currentUser, new Football("pro"), "here",
+				new DayAndTime(new Date(), new Time(3, 0, 0)), "Male", "3"));
+		gArr.add(new Game(currentUser, new Basketball("pro"), "here",
+				new DayAndTime(new Date(), new Time(12, 0, 0)), "Male", "5"));
+		gArr.add(new Game(currentUser, new Basketball("avg"), "here",
+				new DayAndTime(new Date(), new Time(12, 0, 0)), "Male", "5"));
+		gArr.add(new Game(currentUser, new Basketball("nub"), "here",
+				new DayAndTime(new Date(), new Time(12, 0, 0)), "Male", "5"));
+
+		customGridAdapter = new CustomGridViewAdapter(this,
+				R.layout.grid_tile_nub, gArr);
+		gridview.setAdapter(customGridAdapter);
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Toast.makeText(MainActivity.this, "" + position,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+	}
 
 	public void addListenerOnSpinnerItemSelection() {
-  	Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-  	spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-  	Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-  	spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+		Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+		spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void toggleMenu(View v) {
+		this.root.toggleMenu();
+	}
 }
